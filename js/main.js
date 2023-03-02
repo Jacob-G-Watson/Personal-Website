5bebe95e68c46b7cd0104dc9d677ff27fab2accd
Jacob Watson <jacobwatson100@gmail.com>
2023-03-03 08:14:54 +1000

    improving movement of icons

--- js/main.js
+++ js/main.js
@@ -30,23 +30,27 @@ function getFileNames() {
 
 function getFileName() {
 	var fileNames = getFileNames();
+	//todo this random goes through all of the files before selecting the same one twice
 	var fileNameIndex = Math.floor(Math.random() * fileNames.length);
 	return fileNames[fileNameIndex];
 }
 
-function startTheMove() {
+async function startTheMove() {
+	var widthOfIcon = 64;
 	var elements = document.getElementsByClassName("square");
+
 	for (let index = 0; index < elements.length; index++) {
-		myMove(elements[index]);
+		myMove(elements[index], index * widthOfIcon, widthOfIcon);
+		await new Promise((r) => setTimeout(r, 1000));
 	}
 }
 
-function myMove(element) {
-	var widthOfIcon = 64;
+function myMove(element, offSetIconWidth, widthOfIcon) {
+	//todo could css be change to not require the adjustment, when in mobile view there are doubled up icons
 	var topLocation = `${Math.floor(Math.random() * 81) - 40}%`;
-
+	console.log(offSetIconWidth);
 	var id = null;
-	var pos = -innerWidth / 2 - widthOfIcon;
+	var pos = -innerWidth / 2 - offSetIconWidth;
 	clearInterval(id);
 	id = setInterval(frame, 1);
 
@@ -54,9 +58,9 @@ function myMove(element) {
 	element.style.backgroundImage = `url(../images/icons/${getFileName()})`;
 
 	function frame() {
-		if (pos == innerWidth / 2 + widthOfIcon) {
+		if (pos == innerWidth / 2 + offSetIconWidth) {
 			clearInterval(id);
-			myMove(element);
+			myMove(element, offSetIconWidth, widthOfIcon);
 		} else {
 			pos++;
 			element.style.left = pos + "px";
